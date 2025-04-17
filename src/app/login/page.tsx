c546b40d491c345405aaeed317528ca11b7dadcd
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2025-04-17 18:10:03 -0300

    feat: add metadata for login and register pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,24 @@
-import { LoginForm } from "~/components/login-form"
+import { Metadata } from "next";
+import { LoginForm } from "~/components/login-form";
+
+export const metadata: Metadata = {
+  title: "Choosehub - Sign in",
+  description:
+    "Cultural content recommendation platform based on genre. Find new movies, music, and books that match your tastes.",
+  keywords: [
+    "cultural recommendations",
+    "discover movies",
+    "find music",
+    "book suggestions",
+    "cultural platform",
+  ],
+  openGraph: {
+    images: ["/favicon.ico"],
+  },
+  icons: {
+    icon: "/favicon-32x32.png",
+  },
+};
 
 export default function Page() {
   return (
@@ -7,5 +27,5 @@ export default function Page() {
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,24 @@
-import { RegisterForm } from "~/components/register-form"
+import { Metadata } from "next";
+import { RegisterForm } from "~/components/register-form";
+
+export const metadata: Metadata = {
+  title: "Choosehub - Sign Up",
+  description:
+    "Cultural content recommendation platform based on genre. Find new movies, music, and books that match your tastes.",
+  keywords: [
+    "cultural recommendations",
+    "discover movies",
+    "find music",
+    "book suggestions",
+    "cultural platform",
+  ],
+  openGraph: {
+    images: ["/favicon.ico"],
+  },
+  icons: {
+    icon: "/favicon-32x32.png",
+  },
+};
 
 export default function Page() {
   return (
@@ -7,5 +27,5 @@ export default function Page() {
         <RegisterForm />
       </div>
     </div>
-  )
+  );
 }
